c2f3037249661d9c06a8ed96815d904c2eb96765
Johnnyy91 <claudiosardilli@gmail.com>
2022-11-03 15:54:31 +0100

    elaboration file

---
+++ database/migrations/2022_11_03_144942_create_trains_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->id();
+            $table->id();
+            $table->id();
+            $table->id();
+            $table->id();
+            $table->id();
+            $table->id();
+            $table->id();
+
+            Azienda
+           Stazione di partenza
+     Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
